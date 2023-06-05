d0a86fd7beb3522a1a4a408a80988349f748dad5
Miguel Escoz Vilches <kyrovilches@gmail.com>
2023-06-05 19:59:17 +0200

    Perfil usuario terminado

--- app/src/main/java/com/hyperion/dndapiapp/actividades/UserActivity.java
+++ app/src/main/java/com/hyperion/dndapiapp/actividades/UserActivity.java
@@ -5,15 +5,19 @@ import static com.hyperion.dndapiapp.utilidades.Constantes.USUARIO_BUNDLE;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.hyperion.dndapiapp.R;
 import com.hyperion.dndapiapp.databinding.ActivityUserBinding;
 import com.hyperion.dndapiapp.entidades.usuario.Usuario;
+import com.hyperion.dndapiapp.utilidades.Seguridad;
 
 public class UserActivity extends AppCompatActivity {
 
     private Usuario usuario;
+    private Seguridad seguridad;
     private ActivityUserBinding binding;
 
     @Override
@@ -22,6 +26,7 @@ public class UserActivity extends AppCompatActivity {
         binding = ActivityUserBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        seguridad = new Seguridad();
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
             usuario = bundle.getParcelable(USUARIO_BUNDLE);
@@ -33,6 +38,13 @@ public class UserActivity extends AppCompatActivity {
 
     private void iniciaActividad() {
         binding.campoNombreUsuario.setText(usuario.getNombre());
+        binding.campoCorreoUsuario.setText(usuario.getCorreo());
+        try {
+            binding.campoContraseniaUsuario.setText(seguridad.desencriptar(usuario.getContrasenia(), getString(R.string.encoder)));
+
+        } catch (Exception e) {
+            Toast.makeText(this, "No es posible desencriptar la contraseña", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void iniciaListenersBotones() {
@@ -40,6 +52,10 @@ public class UserActivity extends AppCompatActivity {
             Intent intent = new Intent(this, EmptyMainActivity.class);
             intent.putExtra(CIERRA_SESION_BUNDLE, true);
             startActivity(intent);
+            finish();
+        });
+
+        binding.botonAtrasUsuario.setOnClickListener(view -> {
             finish();
         });
     }
